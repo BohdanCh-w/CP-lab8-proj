0067714a47cb658b237a327b88e934ac3fa629a9
Vitailk Shubnik <vental191@gmail.com>
2021-12-01 20:41:08 +0200

    changed classes

--- src/com/company/Emulation.java
+++ src/com/company/Emulation.java
@@ -6,6 +6,58 @@ public class Emulation {
     private Building building;
     private Boolean state;
     private Integer spawnSpeed;
+    private Integer liftSpeed;
     private ArrayList<Thread> liftThreads;
+    private static Emulation emulation;
 
+    private Emulation ( Integer _spawnSpeed, Integer _liftSpeed){
+        this.building = new Building();
+        this.state = false;
+        this.spawnSpeed = _spawnSpeed;
+        this.liftThreads = new ArrayList<>();
+        this.liftSpeed = _liftSpeed;
+    }
+
+    public static Emulation getInstance(Integer ss, Integer ls) {
+        if(emulation == null)
+            emulation = new Emulation(ss,ls);
+        return emulation;
+    }
+    public void SpawnPassenger(){
+
+    }
+    public void Stop(){
+
+    }
+    public void Resume(){
+
+    }
+
+    public void setSpawnSpeed(Integer spawnSpeed) {
+        this.spawnSpeed = spawnSpeed;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+
+    public void setLiftSpeed(Integer liftSpeed) {
+        this.liftSpeed = liftSpeed;
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public Integer getLiftSpeed() {
+        return liftSpeed;
+    }
+
+    public Integer getSpawnSpeed() {
+        return spawnSpeed;
+    }
+
+    public ArrayList<Thread> getLiftThreads() {
+        return liftThreads;
+    }
 }
--- src/com/company/Lift.java
+++ src/com/company/Lift.java
@@ -2,61 +2,22 @@ package com.company;
 
 public class Lift {
     private Integer maxWeight;
-    private Integer maxWidth;
-    private Integer maxLength;
-    private Integer maxHeight;
     private Integer maxPeopleCount;
-    private Float speed;
+
 
     public void moveToFloor(Floor f){
 
     }
-
-    public void setMaxHeight(Integer maxHeight) {
-        this.maxHeight = maxHeight;
-    }
-
-    public void setMaxLength(Integer maxLength) {
-        this.maxLength = maxLength;
-    }
-
     public void setMaxWeight(Integer maxWeight) {
         this.maxWeight = maxWeight;
     }
-
     public void setMaxPeopleCount(Integer maxPeopleCount) {
         this.maxPeopleCount = maxPeopleCount;
     }
-
-    public void setMaxWidth(Integer maxWidth) {
-        this.maxWidth = maxWidth;
-    }
-
-    public void setSpeed(Float speed) {
-        this.speed = speed;
-    }
-
-    public Integer getMaxHeight() {
-        return maxHeight;
-    }
-
     public Integer getMaxWeight() {
         return maxWeight;
     }
-
-    public Integer getMaxLength() {
-        return maxLength;
-    }
-
     public Integer getMaxPeopleCount() {
         return maxPeopleCount;
-    }
-
-    public Integer getMaxWidth() {
-        return maxWidth;
-    }
-
-    public Float getSpeed() {
-        return speed;
     }
 }
