1761cf99545ee7720ef3274f72f3eeced8a3d20c
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-12-08 20:40:16 +0200

    remove warnings. Fuck Fanteak

--- src/com/company/strategy/NoNewStrategy.java
+++ src/com/company/strategy/NoNewStrategy.java
@@ -3,8 +3,6 @@ package com.company.strategy;
 import com.company.Floor;
 import com.company.Lift;
 
-import java.util.ArrayList;
-
 public class NoNewStrategy implements IElevatorStrategy{
     @Override
     public void LoadPassengers(Lift lift, Floor floor){
