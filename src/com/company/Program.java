daa7d1e8500f52b4cdc4c3adc34d393e06b0d48f
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-12-09 14:58:05 +0200

    Add demo version ui

--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -11,14 +11,52 @@ import com.company.ui.UI;
 import com.company.ui.UI;
 
 public class Program {
-    public static Logger logger;
-
-    public Program() {
-        logger = new ConsoleLogger(new FileLogger(new ErrorLogger(null), "logger.txt"));
+    public static void startStop(){
+        isActive = !isActive;
     }
 
+    public static boolean isActive = false;
+    public static Logger logger;
+
     public static void main(String[] args) {
-        Emulation.getInstance();
-        Emulation.getInstance();
+        logger = new ConsoleLogger(new FileLogger(new ErrorLogger(null), "logger.txt"));
+        var ui = new UI();
+
+        ui.addOnStart(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                startStop();
+                if(isActive) {
+                    ((JButton)(e.getSource())).setText("Stop");
+                    System.out.println("Started");
+                } else {
+                    ((JButton)(e.getSource())).setText("Start");
+                    System.out.println("Stopped");
+                }
+            }
+        });
+        
+        ui.addOnClose(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Closed");
+            }
+        });
+        
+        ui.addOnBuild(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int floorCount = ui.Configuration().getFloorsCount();
+                int liftCount = ui.Configuration().getLiftsCount();
+                ui.Building().CreateBuilding(floorCount, liftCount);
+                System.out.println("Built");
+            }
+        });
+
+        try{Thread.sleep(200);} catch(Exception e) {}
+        ui.Building().CreateBuilding(7, 6);
+        ui.Building().SetPassangerNumber(2, 3, 5);
+        try{Thread.sleep(800);} catch(Exception e) {}
+        ui.Building().SetPassangerNumber(2, 3, 7);
+        try{Thread.sleep(800);} catch(Exception e) {}
+        ui.Building().SetPassangerNumber(2, 3, 2);
+        ui.Building().MoveLift(3, 2);
     }
 }
